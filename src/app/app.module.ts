ea5096990820e3baa4bbd7dc83694e21ddb20597
daaren-urthling <daaren.urthling@hotmail.com>
2018-01-28 10:01:53 +0100

    iniziato reenginering con Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { R25LayoutDirective, R25FlexDirective } from './directives/';
-import { LoginComponent } from './login/login.component';
-import { PanelComponent } from './shared/panel.component';
-import { SetupComponent } from './setup/setup.component';
-import { HeaderComponent } from './header/header.component';
-import { FillerComponent } from './shared/filler.component';
 
-import { UserService } from './services';
-
-const ROUTES = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'setup',
-    component: SetupComponent
-  }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    R25LayoutDirective,
-    R25FlexDirective,
-    LoginComponent,
-    PanelComponent,
-    SetupComponent,
-    HeaderComponent,
-    FillerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
   ],
-  providers: [ UserService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
